6c3d36d13122a7951da7615aaa51c13b00cc6a3c
Andrew <andy@andycondon.com>
2017-12-01 16:45:44 +1300

    refine ShowTimes component

--- client/components/ShowTimes.js
+++ client/components/ShowTimes.js
@@ -6,14 +6,23 @@ function ShowTimes ({props}) {
   console.log(props)
   let stop = props.Stop
   let servicesArray = props.Services
+  let expectedDeparture = servicesArray[0].ExpectedDeparture
 
   return (
     <div>
       <p>Leaving from: {stop.Name}</p>
       <p><strong>Last Modified:</strong> <TimeAgo date={props.LastModified} /></p>
-      <p><strong>Expected time:</strong> {servicesArray[0].ExpectedDeparture}</p>
+      <p><strong>Expected time:</strong> {moment(servicesArray[0].ExpectedDeparture).format('h:mm a')} and {moment(servicesArray[0].ExpectedDeparture).format('ss')} seconds</p>
+      <p className="expected">Expected: {servicesArray[0].ExpectedDeparture}</p>
     </div>
   )
+}
+
+function countDown(seconds) {
+  let count = 0 - seconds
+  let intervalId = setInterval(() => {
+    count--
+  }, 1000)
 }
 
 export default ShowTimes
