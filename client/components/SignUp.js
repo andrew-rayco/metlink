4477766b6dc3ec5dbd63a8d59bc22158cb0bc754
Andrew <andy@andycondon.com>
2017-12-18 17:03:11 +1300

    ongoing auth

--- client/components/SignUp.js
+++ client/components/SignUp.js
@@ -38,7 +38,7 @@ class SignUp extends React.Component {
   }
 
   checkUserSignedIn() {
-    firebase.auth().onAuthStateChanged(function(user) {
+    firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         // User is signed in.
         // var displayName = user.displayName;
@@ -63,6 +63,7 @@ class SignUp extends React.Component {
   render() {
     return (
       <div>
+        <h2>Sign up</h2>
         <form>
           <label htmlFor="email">
             Email
@@ -72,7 +73,7 @@ class SignUp extends React.Component {
             Password
             <input id="password" type="text" value={this.state.password} onChange={this.handleChange}/>
           </label>
-          <input type="submit" value="Update" onClick={this.handleClick}/>
+          <input type="submit" value="Submit" onClick={this.handleClick}/>
         </form>
         <p>{this.checkUserSignedIn() ? 'user is logged in as ' + this.state.email : 'Log in'}</p>
       </div>
