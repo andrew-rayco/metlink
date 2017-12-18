ca0ae59a44d6311c83c505ca7b29f3a437e275c3
Andrew <andy@andycondon.com>
2017-12-18 14:19:38 +1300

    add handleChange on edit form

--- client/components/EditStop.js
+++ client/components/EditStop.js
@@ -1,18 +1,38 @@
 import React from 'react'
 
-const EditStop = () => {
-  return (
-    <form>
-      <label for="home">Home stop #
-        <input id="home" type="text" name="home-stop"/>
-        <button id="home-update" value="Update">Update</button>
-      </label>
-      <label for="town">Town stop #
-        <input id="town" type="text" name="town-stop"/>
-        <button type="submit" value="Update">Update</button>
-      </label>
-    </form>
-  )
+class EditStop extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      homeStop: '15',
+      townStop: '1111'
+     }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e) {
+    let id = e.target.id
+    let value = e.target.value
+    this.setState( { [id]: value } )
+  }
+
+  render() {
+    return (
+      <form>
+        <label htmlFor="homeStop">
+          Home stop #
+          <input id="homeStop" type="number" value={this.state.homeStop} onChange={this.handleChange}/>
+          <button id="home-update" value="Update">Update</button>
+        </label>
+        <label htmlFor="townStop">
+          Town stop #
+          <input id="townStop" type="number" value={this.state.townStop} onChange={this.handleChange}/>
+          <button type="submit" value="Update">Update</button>
+        </label>
+      </form>
+    )
+  }
 }
 
 export default EditStop
