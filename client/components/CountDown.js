6dcdb8de4c51e5e8acd9eb229805774008b7d1c2
Andrew <andy@andycondon.com>
2017-12-28 22:18:40 +1300

    formatting

--- client/components/CountDown.js
+++ client/components/CountDown.js
@@ -4,7 +4,9 @@ const CountDown = (props) => {
   return (
     <div className="expected">
       <img src="/img/duration.svg" alt="clock icon"/>
-      <p><span><strong>{props.min}</strong> mins</span> <span><strong>{props.sec}</strong> seconds</span></p>
+      <p>
+        <span><strong>{props.min}</strong> mins</span> <span><strong>{props.sec}</strong> seconds</span>
+      </p>
     </div>
   )
 }
--- client/components/Header.js
+++ client/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
-function Header (props) {
+function Header(props) {
   return (
     <div className="header">
-      <Link to="/"><img src="/img/bus-icon.svg" alt="bus icon"/></Link>
-      <Link to="/"><h1>Sexy Met<span>link</span> App</h1></Link>
+      <Link to="/">
+        <img src="/img/bus-icon.svg" alt="bus icon"/>
+      </Link>
+      <Link to="/">
+        <h1>Sexy Met<span>link</span> App</h1>
+      </Link>
     </div>
   )
 }
