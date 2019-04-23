df89f2628c6474af8f71ce4711c0332011e0e056
Andy Condon <andy.condon@koordinates.com>
2019-04-24 11:37:35 +1200

    upgrade to babel@7, jest testing config okay

---
+++ client/components/ShowTimes.test.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ShowTimes from './ShowTimes'
+
+test('checks that a test works', () => {
+  expect(3).toBe(3)
+})
--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,7 @@ module.exports = {
     loaders: [
       {
         exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react']
-        }
+        loader: 'babel-loader'
       }
     ]
   },
